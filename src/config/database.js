fc5711f0227d566e4942aadc8ac35efa476e5926
muhammed razin <m.razin600@gmail.com>
2025-11-17 14:47:00 +0530

    db correction

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,11 @@ require('dotenv').config()
 const connectdb=async ()=>{
     try{ 
        await mongoose.connect(process.env.mongo_url)
+       return true
     }catch(err){
-        console.error('mongo connection is failed',err.message)
+        console.error('mongo connection is failed due to ',err.message)
+        throw err;
+
         }
 }
 
